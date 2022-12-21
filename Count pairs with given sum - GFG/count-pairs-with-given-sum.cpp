a5ac880e0f5aa350569a3aa74d493d848ce9fd75
murari576 <109016399+murari576@users.noreply.github.com>
2022-12-21 10:26:55 +0530

    Added solution - LeetHub

--- Count pairs with given sum - GFG/count-pairs-with-given-sum.cpp
+++ Count pairs with given sum - GFG/count-pairs-with-given-sum.cpp
@@ -15,7 +15,6 @@ public:
         int cnt = 0;
         
         for(int i = 0; i < n; i++){
-            int val = arr[i];
             int reqval = k - arr[i];
             
             if(mp.find(reqval) != mp.end()){
