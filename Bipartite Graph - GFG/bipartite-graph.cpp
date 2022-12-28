d0d2bea48eae5834a387f24296d6b6276840f609
murari576 <109016399+murari576@users.noreply.github.com>
2022-12-28 16:20:47 +0530

    Added solution - LeetHub

--- Bipartite Graph - GFG/bipartite-graph.cpp
+++ Bipartite Graph - GFG/bipartite-graph.cpp
@@ -5,27 +5,19 @@ using namespace std;
 // } Driver Code Ends
 class Solution {
     private:
-    bool solve(int i, int n, vector<int> adj[], int vis[]){
+    bool solve(int i, int col, vector<int> adj[], int vis[]){
         
-        vis[i] = 0;
-        queue<int> q;
-        q.push(i);
-        
-        while(!q.empty()){
-            int node = q.front();
-            q.pop();
-            
-            for(auto it:adj[node]){
-                if(vis[it] == -1){
-                    vis[it] = !vis[node];
-                    q.push(it);
-                }
-                else if(vis[it] == vis[node])
-                return false;
-            }
-        }
-        return true;
-        
+       vis[i] = col;
+       
+       for(auto it:adj[i]){
+           if(vis[it] == -1){
+               if(solve(it, !col, adj, vis)== false)
+               return false;
+           }
+           else if(vis[it] == col)
+           return false;
+       }
+       return true;
         
     }
 public:
@@ -36,7 +28,7 @@ public:
 	    
 	    for(int i = 0; i < V; i++){
 	        if(vis[i] == -1){
-	            if(solve(i, V, adj, vis) == false)
+	            if(solve(i, 0, adj, vis) == false)
 	            return false;
 	        }
 	    }
