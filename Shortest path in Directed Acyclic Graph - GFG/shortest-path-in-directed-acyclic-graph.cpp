eaefcc29d39fae3262e4d7e294fa6ff610a8fc2d
murari576 <109016399+murari576@users.noreply.github.com>
2023-01-03 11:43:35 +0530

    Added solution - LeetHub

---
+++ Shortest path in Directed Acyclic Graph - GFG/shortest-path-in-directed-acyclic-graph.cpp
@@ -0,0 +1,74 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+#include<bits/stdc++.h>
+class Solution {
+  public:
+     vector<int> shortestPath(int N,int M, vector<vector<int>>& edges){
+
+          vector<pair<int, int> >  adj[N];
+          for(int i = 0; i < M; i++){
+              int u = edges[i][0];
+              int v = edges[i][1];
+              int w = edges[i][2];
+              
+              adj[u].push_back({v,w});
+          }
+          vector<int> dis(N, INT_MAX);
+          int src = 0;
+          dis[src] = 0;
+          queue<int> q;
+          q.push(src);
+          
+          while(!q.empty()){
+              int front = q.front();
+              q.pop();
+              
+              for(auto i:adj[front]){
+                  if(dis[front] + i.second < dis[i.first]){
+                     dis[i.first]  = dis[front] + i.second;
+                      q.push(i.first);
+                  }
+              }
+          }
+          
+         for(int i = 0; i<dis.size(); i++){
+             if(dis[i] == INT_MAX)
+             dis[i] = -1;
+         }
+         return dis;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, m;
+        cin >> n >> m;
+        vector<vector<int>> edges;
+        for(int i=0; i<m; ++i){
+            vector<int> temp;
+            for(int j=0; j<3; ++j){
+                int x; cin>>x;
+                temp.push_back(x);
+            }
+            edges.push_back(temp);
+        }
+        Solution obj;
+        vector<int> res = obj.shortestPath(n, m, edges);
+        for (auto x : res) {
+            cout << x << " ";
+        }
+        cout << "\n";
+    }
+}
+
+// } Driver Code Ends
