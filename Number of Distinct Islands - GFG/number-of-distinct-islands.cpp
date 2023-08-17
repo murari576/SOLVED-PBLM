9668ec0d4c18e7e9ca3f4415be9dadbbe49c7976
MURARI KUMAR JHA <109016399+murari576@users.noreply.github.com>
2023-08-17 20:12:52 +0530

    Added solution - LeetHub

--- Number of Distinct Islands - GFG/number-of-distinct-islands.cpp
+++ Number of Distinct Islands - GFG/number-of-distinct-islands.cpp
@@ -8,42 +8,43 @@ using namespace std;
 // User function Template for C++
 
 class Solution {
-    private:
-    void dfs(int row, int col, vector<vector<int>> &vis, vector<vector<int>> &grid, 
-    vector<pair<int, int>> &vec, int row0, int col0){
-        
+  public:
+    int dx[4] = {1, 0, -1, 0};
+    int dy[4] = {0, -1, 0, 1};
+
+    void solve(int row, int col, vector<vector<bool>> &vis, 
+    vector<vector<int>>& grid, vector<pair<int, int>> &temp, int row0, int col0){
         vis[row][col] = 1;
-        vec.push_back({row-row0, col - col0});
-        
         int n = grid.size();
         int m = grid[0].size();
-        int delx[] = {1, 0, 0, -1};
-        int dely[] = {0, 1, -1, 0};
-        
+        temp.push_back({row-row0, col-col0});
+
         for(int i = 0; i < 4; i++){
-            int nrow = row + delx[i];
-            int ncol = col + dely[i];
-            
-            if(nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && vis[nrow][ncol] == 0 &&
-               grid[nrow][ncol] == 1){
-                   dfs(nrow, ncol, vis, grid, vec, row0, col0);
-               }
+            int nrow = row + dx[i];
+            int ncol = col + dy[i];
+
+            if(nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && 
+            vis[nrow][ncol] == 0 && grid[nrow][ncol] == 1){
+                vis[nrow][ncol] = 1;
+                solve(nrow, ncol, vis, grid, temp, row0, col0);
+            }
         }
-        
     }
-  public:
+    
     int countDistinctIslands(vector<vector<int>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
-        vector<vector<int>> vis(n, vector<int>(m, 0));
+        vector<vector<bool>> vis(n, vector<bool> (m, 0));
+        queue<pair<int, int> > q;
         set<vector<pair<int, int>>> st;
         
+
         for(int i = 0; i < n; i++){
             for(int j = 0; j < m; j++){
                 if(grid[i][j] == 1 && vis[i][j] == 0){
-                    vector<pair<int, int>> vec;
-                    dfs(i, j, vis, grid, vec, i, j);
-                    st.insert(vec);
+                    vector<pair<int, int>> temp;
+                    solve(i, j, vis, grid, temp, i, j);
+                    st.insert(temp);
                 }
             }
         }
