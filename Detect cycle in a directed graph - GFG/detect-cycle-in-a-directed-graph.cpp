aac3858ef915f04bd7c4c73148396da65d5ff8ad
MURARI KUMAR JHA <109016399+murari576@users.noreply.github.com>
2023-08-20 10:22:07 +0530

    Added solution - LeetHub

--- Detect cycle in a directed graph - GFG/detect-cycle-in-a-directed-graph.cpp
+++ Detect cycle in a directed graph - GFG/detect-cycle-in-a-directed-graph.cpp
@@ -6,33 +6,36 @@ using namespace std;
 class Solution {
   public:
     // Function to detect cycle in a directed graph.
-    bool detect(int src, int vis[], int parvis[], vector<int> adj[]){
-        vis[src] = 1;
-        parvis[src] = 1;
-        
-        for(auto it:adj[src]){
-            if(!vis[it]){
-                if(detect(it, vis, parvis, adj) == true)
-                return true;
-            }
-            else if(parvis[it])
-                return true;
-        }
-        parvis[src] = 0;
-        return false;
-    }
     bool isCyclic(int V, vector<int> adj[]) {
         // code here
-        int vis[V] = {0};
-        int parvis[V] = {0};
-        
+        vector<int> indgr(V, 0);
         for(int i = 0; i < V; i++){
-            if(vis[i] == 0){
-                if(detect(i, vis, parvis, adj) == true)
-                return true;
+            for(auto it:adj[i]){
+                indgr[it]++;
             }
         }
+        
+        queue<int> q;
+        for(int i = 0; i < V; i++){
+            if(indgr[i] == 0)
+            q.push(i);
+        }
+        
+        int cnt = 0;
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            
+            cnt++;
+            for(auto it:adj[node]){
+                indgr[it]--;
+                if(indgr[it] == 0)
+                q.push(it);
+            }
+        }
+        if(cnt == V)
         return false;
+        return true;
     }
 };
 
